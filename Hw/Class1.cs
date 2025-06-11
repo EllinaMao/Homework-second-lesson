74514873912bca73505f995538d6d251b3fb5a56
Olga <xotaoshi@gmail.com>
2025-06-12 02:13:44 +0300

    Add project files.

---
+++ Hw/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+      Задание 1
+ Объявить одномерный (5 элементов) массив с именем A и двумерный массив (3 строки, 4 столбца) дробных чисел с именем B. Заполнить одномерный массив
+ А числами, введенными с клавиатуры пользователем, а
+ двумерный массив В случайными числами с помощью
+ циклов. Вывести на экран значения массивов: массива
+ А в одну строку, массива В — в виде матрицы. Найти в
+ данных массивах общий максимальный элемент, минимальный элемент, общую сумму всех элементов, общее
+ произведение всех элементов, сумму четных элементов
+ массива А, сумму нечетных столбцов массива В.
+ */
+namespace Hw
+{
+    internal class MatrixArray
+    {
+
+        static public void RandomMatrix(int[][] matrix)
+        {
+            Random rnd = new Random();
+            int randomint = rnd.Next(0, 25);
+
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = rnd.Next(j);
+                }
+            }
+        }
+    }
+}
---
+++ Hw/SmallArray.cs
@@ -0,0 +1,41 @@
+﻿using homework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+      Задание 1
+ Объявить одномерный (5 элементов) массив с именем A и двумерный массив (3 строки, 4 столбца) дробных чисел с именем B. Заполнить одномерный массив
+ А числами, введенными с клавиатуры пользователем, а
+ двумерный массив В случайными числами с помощью
+ циклов. Вывести на экран значения массивов: массива
+ А в одну строку, массива В — в виде матрицы. Найти в
+ данных массивах общий максимальный элемент, минимальный элемент, общую сумму всех элементов, общее
+ произведение всех элементов, сумму четных элементов
+ массива А, сумму нечетных столбцов массива В.
+ */
+
+namespace Hw
+{
+    internal class SmallArray
+    {
+        public static void InitializeFromUserInput(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine($"Введите элемент {i + 1} из {a.Length}: ");
+                a[i] = homework.UserInput.GetIntFromUser();
+            }
+        }
+
+        public static void OutArray(int[] a)
+        {
+            foreach (int i in a)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}   
